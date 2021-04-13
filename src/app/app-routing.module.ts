eec1501c317465abeb3dda511ad1079141e290de
Robin-Kevin Koppa <rokopp@ttu.ee>
2021-04-13 16:20:50 +0300

    Removed home no use added list and article components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
+import {ListComponent} from './components/list/list.component';
+import {ArticleComponent} from './components/article/article.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+  { path: 'article', component: ArticleComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { ListComponent } from './components/list/list.component';
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    ListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
